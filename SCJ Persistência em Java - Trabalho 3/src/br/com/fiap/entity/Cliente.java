2727578bda864a8454e37fd253dd66f5998319dd
Marcos Reis <marcosdosreis@gmail.com>
2017-12-06 21:21:27 -0200

    Terceiro trabalho.

---
+++ SCJ Persistência em Java - Trabalho 3/src/br/com/fiap/entity/Cliente.java
@@ -0,0 +1,65 @@
+package br.com.fiap.entity;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="CLIENTES")
+public class Cliente implements Serializable {
+	 
+	private static final long serialVersionUID = 1L;
+	@Id
+	@Column(name="IDCLIENTE")
+	private int id;
+	
+	@Column(name="NOME", length=45)
+	private String nome;
+	
+	@Column(name="EMAIL", length=45)
+	private String email;
+	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "cliente", orphanRemoval = true)
+	private List<Pedido> pedidos = new ArrayList<Pedido>();
+
+	public int getId() {
+		return id;
+	}	
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+
+
+}
---
+++ SCJ Persistência em Java - Trabalho 3/src/br/com/fiap/program/TesteAplicacao.java
@@ -0,0 +1,106 @@
+package br.com.fiap.program;
+
+import java.util.Date;
+import java.util.List;
+
+import br.com.fiap.dao.GenericDAO;
+import br.com.fiap.entity.Cliente;
+import br.com.fiap.entity.Pedido;
+
+public class TesteAplicacao {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		try {
+			GenericDAO<Cliente> dao = new GenericDAO<>(Cliente.class);
+
+			//Definindo o primeiro cliente
+
+			Cliente cliente = new Cliente();
+			cliente.setId(10);
+			cliente.setNome("JULIO ALMEIDA");
+			cliente.setEmail("julio@mail.com");
+			
+			//Definindo dois pedidos
+			Pedido pedido = new Pedido();
+			pedido.setId(1001);
+			pedido.setCliente(cliente);
+			pedido.setData(new Date());
+			pedido.setDescricao("Pedido 1 do Cliente 10");
+			pedido.setValor(100.10);
+			cliente.getPedidos().add(pedido);
+
+			pedido = new Pedido();
+			pedido.setId(1002);
+			pedido.setCliente(cliente);
+			pedido.setData(new Date());
+			pedido.setDescricao("Pedido 2 do Cliente 10");
+			pedido.setValor(200.20);
+			cliente.getPedidos().add(pedido);
+			
+			dao.adicionar(cliente);
+			
+			System.out.println("Primeiro cliente inserido com sucesso!");
+
+			//Definindo o segundo cliente
+
+			cliente = new Cliente();
+			cliente.setId(20);
+			cliente.setNome("SANDRA ALBUQUERQUE");
+			cliente.setEmail("xxxxx@mail.com");
+			
+			//Definindo único pedido
+			pedido = new Pedido();
+			pedido.setId(2001);
+			pedido.setCliente(cliente);
+			pedido.setData(new Date());
+			pedido.setDescricao("Pedido 1 do Cliente 20");
+			pedido.setValor(300.30);
+			cliente.getPedidos().add(pedido);
+		
+			dao.adicionar(cliente);
+
+			System.out.println("Segundo cliente inserido com sucesso!");
+
+			//Listar clientes
+			listarClientes(dao.listar());
+			
+			//Atualizar primeiro cliente
+			cliente = dao.buscar(20);
+			cliente.setEmail("sandra@mail.com");
+			dao.atualizar(cliente);
+
+			System.out.println("Primeiro cliente alterado com sucesso!");
+			
+			//Listar clientes
+			listarClientes(dao.listar());
+
+			//Remover primeiro cliente
+			cliente = dao.buscar(10);
+			dao.remover(cliente);
+
+			System.out.println("Segundo cliente excluído com sucesso!");
+
+			//Listar clientes
+			listarClientes(dao.listar());
+			
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void listarClientes(List<Cliente> lista) {
+		System.out.println("--- Clientes / Pedidos ---");
+		for(Cliente c : lista){
+			System.out.println("Cliente: " + c.getId() + " Nome: " + c.getNome() + " Email: " + c.getEmail());
+			for(Pedido p : c.getPedidos()) {
+				System.out.println("    Pedido: " + p.getId() + " Data: " + p.getData() + " Descrição: " + p.getDescricao() + " Valor: " + p.getValor());
+			}
+		}	
+	}
+}
+
+
+
